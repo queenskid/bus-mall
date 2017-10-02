42b5b326a7bb08c335ae562aabb3abf5a637847a
queenskid <andres.ibarra0716@gmail.com>
2017-10-02 15:22:02 -0700

    pictures are displaying on browser

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
-'use strict'
+'use strict';
 
 // array to store the objects, but also need an array within with 3 images
-Photo.allPhotos = [];
+Photo.allPics = [];
 
 //make an object
-function Photo(name, filepath, votes, views) {
+function Photo(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  this.votes = votes;
-  this.views = views;
-  this.
+  // this.votes = votes;
+  // this.views = views;
+  // this.
+  // this.
+  Photo.allPics.push(this);
 
-  Photo.allPhotos.push(this);
-
-}// make new instances
-new Photo('bag', './img/bag.jpg');
-new Photo('banana', './img/banana.jpg');
-new Photo('bathroom', './img/bathroom.jpg');
-new Photo('boots', './img/boots.jpg');
-new Photo('breakfast', './img/breakfast.jpg');
-new Photo('bubblegum', './img/bubblegum.jpg');
-new Photo('chair', './img/chair.jpg');
-new Photo('cthulhu', './img/cthulhu.jpg');
-new Photo('dog-duck', './img/dog-duck.jpg');
-new Photo('dragon', './img/dragon.jpg');
-new Photo('pen', './img/pen.jpg');
-new Photo('pet-sweep', './img/pet-sweep.jpg');
-new Photo('scissors', './img/scissors.jpg');
-new Photo('shark', './img/shark.jpg');
-new Photo('sweep', './img/sweep.png');
-new Photo('tauntaun', './img/tauntaun.jpg');
-new Photo('unicorn', './img/unicorn.jpg');
-new Photo('usb', './img/usb.gif');
-new Photo('water-can', './img/water-can.jpg');
-new Photo('wine-glass', './img/wine-glass.jpg');
+}
+// make new instances
+new Photo('bag', 'images/bag.jpg');
+new Photo('banana', 'images/banana.jpg');
+new Photo('bathroom', 'images/bathroom.jpg');
+new Photo('boots', 'images/boots.jpg');
+new Photo('breakfast', 'images/breakfast.jpg');
+new Photo('bubblegum', 'images/bubblegum.jpg');
+new Photo('chair', 'images/chair.jpg');
+new Photo('cthulhu', 'images/cthulhu.jpg');
+new Photo('dog-duck', 'images/dog-duck.jpg');
+new Photo('dragon', 'images/dragon.jpg');
+new Photo('pen', 'images/pen.jpg');
+new Photo('pet-sweep', 'images/pet-sweep.jpg');
+new Photo('scissors', 'images/scissors.jpg');
+new Photo('shark', 'images/shark.jpg');
+new Photo('sweep', 'images/sweep.png');
+new Photo('tauntaun', 'images/tauntaun.jpg');
+new Photo('unicorn', 'images/unicorn.jpg');
+new Photo('usb', 'images/usb.gif');
+new Photo('water-can', 'images/water-can.jpg');
+new Photo('wine-glass', 'images/wine-glass.jpg');
+console.log(Photo.allPics);
 
 // listener, and somthing to be clicked
-var imgEl = document.getElementById('photos');
 
-imgEl.addEventListener('click',randomPhoto);
+
+var imgEl = document.getElementById('pics');
+
+imgEl.addEventListener('click', randomPhoto);
 
 // randomly display one of the picture
-fucntion randomPhoto(){
-  var randomIndex = Math.floor(Math.random() * Photo.allPhotos.length);
-  imgEl.src = Photo.allPhotos[randomIndex].filepath;
+function randomPhoto(){
+  var randomIndex = Math.floor(Math.random() * Photo.allPics.length);
+  imgEl.src = Photo.allPics[randomIndex].filepath;
 }
 
 randomPhoto();
