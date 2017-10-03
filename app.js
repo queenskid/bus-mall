b6cd7fa7fe1da87e25f70304c19cee6b6b1aafdb
queenskid <andres.ibarra0716@gmail.com>
2017-10-02 20:57:18 -0700

    3 picture display almost complete

--- app.js
+++ app.js
@@ -1,42 +1,82 @@
 'use strict';
 
-// array to store the objects, but also need an array within with 3 images
-Photo.allPics = [];
-
-//make an object
 function Photo(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  // this.votes = votes;
-  // this.views = views;
-  // this.
-  // this.
-  Photo.allPics.push(this);
+  this.votes = 0;
+  this.views = 0;
+};
 
+Photo.prototype.clicked = function(){
+  this.votes++;
+};
+
+Photo.prototype.viewed = function(){
+  this.views++;
+};
+
+var official = [];
+var totalClicks = 0;
+var allPics = [
+  new Photo('bag', 'images/bag.jpg'),
+  new Photo('banana', 'images/banana.jpg'),
+  new Photo('bathroom', 'images/bathroom.jpg'),
+  new Photo('boots', 'images/boots.jpg'),
+  new Photo('breakfast', 'images/breakfast.jpg'),
+  new Photo('bubblegum', 'images/bubblegum.jpg'),
+  new Photo('chair', 'images/chair.jpg'),
+  new Photo('cthulhu', 'images/cthulhu.jpg'),
+  new Photo('dog-duck', 'images/dog-duck.jpg'),
+  new Photo('dragon', 'images/dragon.jpg'),
+  new Photo('pen', 'images/pen.jpg'),
+  new Photo('pet-sweep', 'images/pet-sweep.jpg'),
+  new Photo('scissors', 'images/scissors.jpg'),
+  new Photo('shark', 'images/shark.jpg'),
+  new Photo('sweep', 'images/sweep.png'),
+  new Photo('tauntaun', 'images/tauntaun.jpg'),
+  new Photo('unicorn', 'images/unicorn.jpg'),
+  new Photo('usb', 'images/usb.gif'),
+  new Photo('water-can', 'images/water-can.jpg'),
+  new Photo('wine-glass', 'images/wine-glass.jpg')
+];
+
+var photoDisplay = function() {
+  var current = [];
+  while(current.length < 3) {
+    var photo = randomPhoto();
+    var duplicate = false;
+    for(var i = 0; i < official.length; i++) {
+      if(official[i].name == photo.name) {
+        duplicate = true;
+      }
+    }
+
+    if(!duplicate) {
+      for(var j = 0; j < current.length; j++) {
+        if(current[j].name == photo.name) {
+          duplicate = true;
+        }
+      }
+    }
+
+    if(!duplicate) {
+      photo.viewed();
+      current.push(photo);
+    }
+  }
+
+  document.getElementById("pic1").src = current[0].filepath;
+  document.getElementById("pic2").src = current[1].filepath;
+  document.getElementById("pic3").src = current[2].filepath;
+
+  document.getElementById("pic1").alt = current[0].name;
+  document.getElementById("pic2").alt = current[1].name;
+  document.getElementById("pic3").alt = current[2].name;
+  official = current;
 }
-// make new instances
-new Photo('bag', 'images/bag.jpg');
-new Photo('banana', 'images/banana.jpg');
-new Photo('bathroom', 'images/bathroom.jpg');
-new Photo('boots', 'images/boots.jpg');
-new Photo('breakfast', 'images/breakfast.jpg');
-new Photo('bubblegum', 'images/bubblegum.jpg');
-new Photo('chair', 'images/chair.jpg');
-new Photo('cthulhu', 'images/cthulhu.jpg');
-new Photo('dog-duck', 'images/dog-duck.jpg');
-new Photo('dragon', 'images/dragon.jpg');
-new Photo('pen', 'images/pen.jpg');
-new Photo('pet-sweep', 'images/pet-sweep.jpg');
-new Photo('scissors', 'images/scissors.jpg');
-new Photo('shark', 'images/shark.jpg');
-new Photo('sweep', 'images/sweep.png');
-new Photo('tauntaun', 'images/tauntaun.jpg');
-new Photo('unicorn', 'images/unicorn.jpg');
-new Photo('usb', 'images/usb.gif');
-new Photo('water-can', 'images/water-can.jpg');
-new Photo('wine-glass', 'images/wine-glass.jpg');
-console.log(Photo.allPics);
 
+photoDisplay();
+/*
 // listener, and somthing to be clicked
 
 
@@ -45,12 +85,30 @@ var imgEl = document.getElementById('pics');
 imgEl.addEventListener('click', randomPhoto);
 
 // randomly display one of the picture
+*/
 function randomPhoto(){
-  var randomIndex = Math.floor(Math.random() * Photo.allPics.length);
-  imgEl.src = Photo.allPics[randomIndex].filepath;
+  var randomIndex = Math.floor(Math.random() * allPics.length);
+  return allPics[randomIndex];
 }
 
-randomPhoto();
+function switchPhoto(element) {
+  var clicked_photo = getPhotoByName(element.alt);
+  clicked_photo.clicked();
+  console.log(clicked_photo);
+  if(totalClicks < 25) {
+    photoDisplay();
+    totalClicks++;
+  } else {
+    alert("no sea fresco!")
+  }
+}
 
-
-// (name, path, votes, views)
+function getPhotoByName(name) {
+  var photo = null;
+  allPics.forEach(function(item) {
+    if(item.name == name) {
+      photo = item;
+    }
+  })
+  return photo;
+}
